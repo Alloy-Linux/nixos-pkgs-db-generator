ddda6d5f021056d0919ea42c42719e8dcf9b810d
Simon-Weij <simongvanderweij@gmail.com>
2025-08-26 14:45:19 +0200

    Added nix build command

--- flake.nix
+++ flake.nix
@@ -15,15 +15,35 @@
           default = pkgs.mkShell {
             packages = [
               pkgs.gcc
-			  pkgs.sqlite
-			  pkgs.cjson
-			  pkgs.pkg-config
+              pkgs.sqlite
+              pkgs.cjson
+              pkgs.pkg-config
             ];
 
             shellHook = ''
               echo "Development environment loaded"
             '';
           };
+        };
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          pname = "nixos-pkgs-db-generator";
+          version = "0.0.1";
+          src = ./.;
+
+          buildInputs = [ 
+            pkgs.gcc
+            pkgs.sqlite
+            pkgs.cjson
+            pkgs.pkg-config
+          ];
+
+          buildPhase = ''
+            gcc -o SQLGenerator main.c -lsqlite3 -lcjson
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp SQLGenerator $out/bin/nixos-pkgs-db-generator
+          '';
         };
       }
     );
